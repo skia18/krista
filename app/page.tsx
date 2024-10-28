bddd66cf70af453df3661fdf2707405439346c73
Magnus-18 <satishvijay1080@outlook.com>
2024-10-28 20:03:34 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="grid grid-cols-2 h-full w-full">
+      <div className="">
+          Image
+      </div>
+      <div className="">
+          Auth section
+      </div>
+
+    </div>
+  );
+}
